bf6ce53fd2d0bcf44b1d11337465b3b696a29e9f
CMLTTN <cemaldim@gmail.com>
2023-07-28 20:16:11 +0300

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import { HomeContainer, HomeImg } from "./Home.style";
+import Header from "../../components/header/Header";
 
 const Home = () => {
+  const printType = ["all", "books", "magazines"];
+  const [query, setQuery] = useState(0);
+  const [selectedType, setSelectedType] = useState("all")
   return (
-    <div>Home</div>
-  )
-}
+    <HomeContainer>
+      <Header setQuery={setQuery} setSelectedType={selectedType}/>
+      <HomeImg></HomeImg>
+    </HomeContainer>
+  );
+};
 
-export default Home
+export default Home;
