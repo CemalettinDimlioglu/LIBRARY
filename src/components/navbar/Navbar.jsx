5cb9d0120b09337882cc9ca07391c758bac5fa79
CMLTTN <cemaldim@gmail.com>
2023-07-28 20:16:04 +0300

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,40 @@
  import React, { useState } from "react";
  import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
+
  import { menuIcon } from "../../helper/iconData";
- const Navbar = () => {
-   const [showMenu, setshowMenu] = useState(false);
+
+
+ const Navbar = ({ currentUser, setCurrentUser }) => {
+   const [showMenu, setShowMenu] = useState(false);
+   const logout=()=>{
+  setCurrentUser(false);
+sessionStorage.clear() ; 
+  
+  }
+
+
    return (
      <Nav justify="space-between" wrap="wrap">
-       <Logo to="">Clarus Library</Logo>
-       <HamburgerIcon onClick={() => setshowMenu(!showMenu)}>
+       <Logo to="/"> Clarus Library </Logo>
+       <HamburgerIcon onClick={() => setShowMenu(!showMenu)}>
          {menuIcon}
        </HamburgerIcon>
        <Menu showMenu={showMenu}>
-         <MenuLink to="/">Home</MenuLink>
-         <MenuLink to="/about">About</MenuLink>
-         <MenuLink to="/register">Register</MenuLink>
-         <MenuLink to="login">Login</MenuLink>
-         <MenuLink to="/login">Logout</MenuLink>
+         <MenuLink to="/"> Home</MenuLink>
+         <MenuLink to="/about"> About</MenuLink>
+         {currentUser ? (
+           <MenuLink to="/login" onClick={logout} > Logout</MenuLink>
+         ) : (
+           <>
+             <MenuLink to="/register"> Register</MenuLink>
+             <MenuLink to="/login">Login</MenuLink>
+           </>
+         )}
+         {/* <MenuLink to="/register"> Register</MenuLink>
+  <MenuLink to="/login">Login</MenuLink>
+  <MenuLink to="/login"> Logout</MenuLink>  */}
        </Menu>
      </Nav>
    );
  };
  export default Navbar;
-
-
-
-
-
-
-
